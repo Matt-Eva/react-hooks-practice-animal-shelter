60a1b807ba226ee8b324e3e1863455f28f93b954
Matt Eva <matteweva@gmail.com>
2021-11-11 15:33:03 -0800

    commit tree 25b3bb747e65a3b2c75a6cca659533e919852d56

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,60 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+  const url = "http://localhost:3001/pets"
+
+  function onChangeType(e) {
+    console.log(e.target.value)
+    setFilters({type: e.target.value})
+  }
+
+  function onFindPetsClick() {
+    if (filters.type === "all"){
+      fetch(url)
+        .then(r => r.json())
+        .catch(error => console.error(error))
+        .then(data => setPets([...data]))
+    } else{
+      fetch(url + `?type=${filters.type}`)
+      .then(r => r.json())
+      .catch(error => console.error(error))
+      .then(data => setPets([... data]))
+    }
+  }
+
+  function onAdoptPet(id) {
+    const oneAdopted = pets.map(pet =>{
+      if (pet.id === id){
+        const adopted = {...pet, isAdopted: true}
+        return adopted
+      } else{
+        return pet
+      }
+    })
+    setPets([...oneAdopted])
+
+    // const configObj = {
+    //   method: "PATCH",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   }, 
+    //   body: JSON.stringify({isAdopted: true})
+    // }
+
+    // fetch(url + `/${id}`, configObj)
+    // .then(r => r.json())
+    // .catch(error => console.error(error))
+    // .then(data => {
+    //   const oneAdopted = pets.map(pet =>{
+    //     if (pet.id === data.id){
+    //       return data;
+    //     } else{
+    //       return pet;
+    //     }
+    //   })
+    //   setPets([...oneAdopted])
+    // })
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +69,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
